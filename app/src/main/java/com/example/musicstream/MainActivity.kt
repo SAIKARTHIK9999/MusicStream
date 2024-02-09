d29fe2e460e9af1e7d5c8894d7d3dc4c431ccdd8
SAIKARTHIK9999 <karthikmumadi@outlook.com>
2024-02-09 13:33:18 +0530

    redisgnHomeScreen

--- app/src/main/java/com/example/musicstream/MainActivity.kt
+++ app/src/main/java/com/example/musicstream/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.example.musicstream
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.RelativeLayout
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Adapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import com.example.musicstream.adapter.CategoryAdapter
+import com.example.musicstream.adapter.SectionSongListAdapter
 import com.example.musicstream.databinding.ActivityMainBinding
 import com.example.musicstream.databinding.CategoryItemRecyclerRowBinding
 import com.example.musicstream.models.CategoryModel
 import com.google.common.io.LineReader
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.toObject
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,10 +31,33 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         getCategories()
+        setupSection("section_1",binding.section1MainLayout,binding.section1Title,binding.section1RecyclerView)
+        setupSection("section_2",binding.section2MainLayout,binding.section2Title,binding.section2RecyclerView)
+        setupSection("section_3",binding.section3MainLayout,binding.section3Title,binding.section3RecyclerView)
 
 
     }
 
+    override fun onResume() {
+        super.onResume()
+        showPlayerView()
+    }
+
+    fun showPlayerView(){
+        binding.playerView.setOnClickListener {
+            startActivity(Intent(this,PlayerActivity::class.java))
+        }
+        MyExoplayer.getCurrentSong()?.let {
+            binding.playerView.visibility = View.VISIBLE
+            binding.songTitleTextView.text = "Now Playing : " + it.title
+            Glide.with(binding.songCoverImageView).load(it.coverUrl)
+                .apply(
+                    RequestOptions().transform(RoundedCorners(32))
+                ).into(binding.songCoverImageView)
+        } ?: run{
+            binding.playerView.visibility = View.GONE
+        }
+    }
 
     fun getCategories(){
         FirebaseFirestore.getInstance().collection("category")
@@ -40,6 +73,24 @@ class MainActivity : AppCompatActivity() {
         binding.categoriesRecyclerView.adapter = categoryAdapter
     }
 
+    fun setupSection(id : String, mainLayout : RelativeLayout, titleView : TextView, recyclerView: RecyclerView){
+        FirebaseFirestore.getInstance().collection("sections")
+            .document(id)
+            .get().addOnSuccessListener {
+                val section = it.toObject(CategoryModel::class.java)
+                section?.apply {
+                    mainLayout.visibility = View.VISIBLE
+                    titleView.text = name
+                    recyclerView.layoutManager = LinearLayoutManager(this@MainActivity,LinearLayoutManager.HORIZONTAL,false)
+                    recyclerView.adapter = SectionSongListAdapter(songs)
+                    mainLayout.setOnClickListener {
+                        SongsListActivity.category = section
+                        startActivity(Intent(this@MainActivity,SongsListActivity::class.java))
+                    }
+                }
+            }
+
+    }
 
 
 }
